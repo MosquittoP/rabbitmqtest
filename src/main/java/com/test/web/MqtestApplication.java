c6d37c71dd1328cdb2b4f1211d1f98b1b90c0019
MosquittoP <ysm00813@gmail.com>
2022-04-22 19:43:29 +0900

    최초 프로젝트

---
+++ src/main/java/com/test/web/MqtestApplication.java
@@ -0,0 +1,13 @@
+package com.test.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/web/MqtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
